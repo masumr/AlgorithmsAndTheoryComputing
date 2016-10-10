24705a995e5caa16c0ea11f062805c3f0a16e98d
Mayur Tolani <mayur.tolani54@gmail.com>
2016-10-10 16:08:35 -0400

    5A Done

--- week 5/programming Assignment/CS6085ATolani.java
+++ week 5/programming Assignment/CS6085ATolani.java
@@ -1,36 +1,67 @@
 import java.util.*;
+import java.text.*;
 class CS6085ATolani {
+	static Lab5BTMethods one = new Lab5BTMethods();
+	static int array[] ;
+	static int counter=0;
+	
 	public static void main(String[] args) {
-		BTMethods one = new BTMethods();
+		new MyInfo().identity();
 		one.createBinaryTree();
+		array = new int[numberOfNodes(one.root)];
 		System.out.println();
 		System.out.println("Pre Order Travesal");
 		one.preOrder(one.root);
-		System.out.println();
-		System.out.println("height = "+one.height(one.root));
-		System.out.print("level Order");
+		System.out.println("\n");
+		System.out.println("Height of the Tree = "+one.height(one.root));
+		System.out.print("\nThe Level Order of the Tree");
 		one.displayTree(one.root);
-		System.out.println();
-		System.out.println("number of nodes in the tree : "+numberOfNodes(one.root));
-
+		System.out.println("\n");
+		System.out.println("Number of nodes in the tree : "+numberOfNodes(one.root));
+		System.out.println("\nLargest Value in the tree : "+largest(one.root));
+		System.out.println("\n");
 	}
-	static int numberOfNodes(BTNode root){
+	static int numberOfNodes(Lab5BTNode root){
 		if(root == null)
 			return 0;
 		else 
 			return(numberOfNodes(root.right) + numberOfNodes(root.left) + 1);
 	}
-	static void largest(BTNode root){
-		char currentChar = 'a';
+	static int largest(Lab5BTNode root){
+		int temp = 0;
 		if(root!=null)
 		{
-			if(root.element > currentChar)
-			{
-				currentChar = root.element();
-			}
+			array[counter++] = root.element;
 			largest(root.left);
 			largest(root.right);
 		}
-		System.out.println(currentChar);
+		for (int i =0; i<array.length; i++) {
+			if(array[i]>temp){
+				temp = array[i];
+			}
+		}
+		return temp;
 	}
+	
+}
+class MyInfo{
+  String todaysDate = "06/OCT/2016";
+  String assignmentNumber = "5A";
+
+  public void identity(){
+  // My info and class and assigmnegt date and all
+    System.out.println("\n");
+    System.out.println("Name : Mayur Tolani");
+    System.out.println("Course : CS608");
+    SimpleDateFormat dateformat3 = new SimpleDateFormat("dd/MMM/yyyy");
+    Date assingmentDate = null;
+    try{
+        assingmentDate = dateformat3.parse(todaysDate); //Date printing, using the DATE class in java.
+      } 
+      catch (ParseException ex) {
+          System.out.print(ex);
+      }
+    System.out.println("Assingment " +assignmentNumber+ "\nDone on : "+dateformat3.format(assingmentDate));
+    System.out.println("--------------------------------------------------------");
+  }
 }
--- week 5/programming Assignment/CS6085BTolani.java
+++ week 5/programming Assignment/CS6085BTolani.java
@@ -1,48 +1,160 @@
 import java.util.*;
+import java.text.*;
 class CS6085BTolani {
-	public static void main(String[] args) {
-		BTMethods one = new BTMethods();
+	
+	static Lab5BTMethods one = new Lab5BTMethods();
+	static int array[] ;
+	static int counter=0;
+	static int level = 0;
+	static int right = 0;
+	static int left  = 0;
+	static int numberOfNodesInLevel = 0;
+	static int levelWithMaxNodes = -1;
+
+	public static void main(String[] args) 
+	{
+		new MyInfo().identity();
 		one.createBinaryTree();
+		array = new int[numberOfNodes(one.root)];
 		System.out.println();
 		System.out.println("Pre Order Travesal");
 		one.preOrder(one.root);
-		System.out.println();
-		System.out.println("height = "+one.height(one.root));
-		System.out.print("level Order");
+		System.out.println("\n");
+		System.out.println("Height of the Tree = "+one.height(one.root));
+		System.out.print("\nThe Level Order of the Tree");
 		one.displayTree(one.root);
+		System.out.println("\n");
+		System.out.println("Number of nodes in the tree : "+numberOfNodes(one.root));
+		System.out.println("\nLargest Value in the tree : "+largest(one.root));
 		System.out.println();
-		System.out.println("number of nodes in the tree : "+numberOfNodes(one.root));
-
+		System.out.println("Sum of Elements : " + sumOfElements(one.root));
+		System.out.println();
+		int x = 10;//search element
+		System.out.println("Search for Number " + x +" : "+searchFor(one.root,x));
+		System.out.println();
+		setLargestNumberOfNodes(one.root);
+		level=0;
+		levelWithLargestNumberOfNodes(one.root);
+		//System.out.println("Max Number of Nodes in any Level : "+(numberOfNodesInLevel+1));
+		//System.out.println(levelWithMaxNodes);
 	}
-	static int numberOfNodes(BTNode root){
+	static int numberOfNodes(Lab5BTNode root)
+	{
 		if(root == null)
 			return 0;
 		else 
 			return(numberOfNodes(root.right) + numberOfNodes(root.left) + 1);
 	}
-	static void largest(BTNode root){
-		char currentChar = 'a';
+	static void toArray(Lab5BTNode root)
+	{
+
 		if(root!=null)
 		{
-			if(root.element > currentChar)
-			{
-				currentChar = root.element();
-			}
-			largest(root.left);
-			largest(root.right);
+			array[counter++] = root.element;
+			toArray(root.left);
+			toArray(root.right);
 		}
-		System.out.println(currentChar);
 	}
-	static int sumOfElements()
+	static int largest(Lab5BTNode root)
 	{
+		int large = 0;
+		counter=0;
+		toArray(root);
+		for (int i =0; i<array.length; i++) {
+			if(array[i]>large){
+				large = array[i];
+			}
+		}
+		return large;
+	}
+	
+	// program for Assignment B
+	static int sumOfElements(Lab5BTNode root)
+	{
+		counter=0;
+		toArray(root);
+		int sum=0;
+		for (int i = 0; i < array.length; i++) {
+			sum = sum + array[i];
+		}
+		return sum;
+	}
+	static boolean searchFor(Lab5BTNode root, int n)
+	{
+		counter=0;
+		toArray(root);
+		for (int i =0; i<array.length; i++)
+			if(array[i]==n)
+				return true;
+		return false;
+	}
+	static void setLargestNumberOfNodes(Lab5BTNode root)
+	{
+		right=0;
+		left = 0;
+		if (root == null) return;
+		else
+		{
+			setLargestNumberOfNodes(root.right);
+			right++;
+			
+			setLargestNumberOfNodes(root.left);
+			left++;
+			level++;
+			
+			if((left+right) > numberOfNodesInLevel)
+			{
+				numberOfNodesInLevel = left+right;
+
+			}
+
+		}
 
 	}
-	static boolean searchFor( char n )
+	static void levelWithLargestNumberOfNodes(Lab5BTNode root)
 	{
+		right=0;
+		left = 0;
+		if (root == null) return;
+		else
+		{
+			levelWithLargestNumberOfNodes(root.right);
+			right++;
+			
+			levelWithLargestNumberOfNodes(root.left);
+			left++;
+			level++;
+			
+			if((left+right) == numberOfNodesInLevel)
+			{
+				System.out.println("level With largest Nodes: "+ (level));
+
+			}
+
+		}
 
 	}
-	static int levelWithLargestNumNodes(BTNode root)
-	{
-		
-	}
+	
+}
+class MyInfo
+{
+  String todaysDate = "06/OCT/2016";
+  String assignmentNumber = "5B";
+
+  public void identity(){
+  // My info and class and assigmnegt date and all
+    System.out.println("\n");
+    System.out.println("Name : Mayur Tolani");
+    System.out.println("Course : CS608");
+    SimpleDateFormat dateformat3 = new SimpleDateFormat("dd/MMM/yyyy");
+    Date assingmentDate = null;
+    try{
+        assingmentDate = dateformat3.parse(todaysDate); //Date printing, using the DATE class in java.
+      } 
+      catch (ParseException ex) {
+          System.out.print(ex);
+      }
+    System.out.println("Assingment " +assignmentNumber+ "\nDone on : "+dateformat3.format(assingmentDate));
+    System.out.println("--------------------------------------------------------");
+  }
 }
